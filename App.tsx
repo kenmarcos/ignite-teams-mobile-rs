8440a19d493c2fd8197b04b4cfda5690d73f06c4
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2025-02-02 11:03:53 -0300

    CSS-in-JS: Fontes personalizadas e componente Loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
+import { LoadIndicator } from "@/components/Loading/styles";
 import { Groups } from "@/screens/Groups";
 import { theme } from "@/styles/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <LoadIndicator />}
     </ThemeProvider>
   );
 }
