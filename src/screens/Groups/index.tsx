0986b632f4d313c5eb5a0958e71a81e541c82161
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2025-02-02 11:37:44 -0300

    CSS-in -JS: Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@/components/Header";
 import { Container } from "./styles";
+import { HighLight } from "@/components/HighLight";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
