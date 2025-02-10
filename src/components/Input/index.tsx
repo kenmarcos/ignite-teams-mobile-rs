e9a5f5ad7081b200e7c4ec40eeb013379f79701f
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2025-02-10 14:05:58 -0300

    CSS-in-JS: Criando Componente Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "./styles";
+import { TextInputProps } from "react-native";
+
+export const Input = ({ ...rest }: TextInputProps) => {
+  return <Container {...rest} />;
+};
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Button } from "@/components/Button";
 import { HighLight } from "@/components/HighLight";
+import { Input } from "@/components/Input";
 
 export const NewGroup = () => {
   return (
@@ -16,7 +17,9 @@ export const NewGroup = () => {
           subtitle="Crie a turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
